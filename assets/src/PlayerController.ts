99f74c50576fa6e1f46b0f35c379b55f6e728104
OSA\00004980 <fulande@osa.com>
2023-04-04 11:35:25 +0800

    feat: dfdfff

--- assets/src/PlayerController.ts
+++ assets/src/PlayerController.ts
@@ -1,48 +1,61 @@
-import { _decorator, Component, Node, input, Input, EventMouse, Vec3 } from 'cc'
-const { ccclass, property } = _decorator
-export const BLOCK_SIZE = 40 // 添加一个放大比
+import {
+  _decorator,
+  Component,
+  Node,
+  input,
+  Input,
+  EventMouse,
+  Vec3,
+} from "cc";
+const { ccclass, property } = _decorator;
+export const BLOCK_SIZE = 40; // 添加一个放大比
 
-@ccclass('PlayerController')
+@ccclass("PlayerController")
 export class PlayerController extends Component {
-  private _startJump: boolean = false
-  private _jumpStep: number = 0
-  private _curJumpTime: number = 0
-  private _jumpTime: number = 0.3
-  private _curJumpSpeed: number = 0
-  private _curPos: Vec3 = new Vec3()
-  private _deltaPos: Vec3 = new Vec3(0, 0, 0)
-  private _targetPos: Vec3 = new Vec3()
+  private _startJump: boolean = false;
+  private _jumpStep: number = 0;
+  private _curJumpTime: number = 0;
+  private _jumpTime: number = 0.3;
+  private _curJumpSpeed: number = 0;
+  private _curPos: Vec3 = new Vec3();
+  private _deltaPos: Vec3 = new Vec3(0, 0, 0);
+  private _targetPos: Vec3 = new Vec3();
 
   start() {
-    input.on(Input.EventType.MOUSE_UP, this.onMouseUp, this)
+    console.log(17);
+    input.on(Input.EventType.MOUSE_UP, this.onMouseUp, this);
   }
   update(deltaTime: number) {
     if (this._startJump) {
-      this._curJumpTime += deltaTime // 累计总的跳跃时间
+      this._curJumpTime += deltaTime; // 累计总的跳跃时间
       if (this._curJumpTime > this._jumpTime) {
         // 当跳跃时间是否结束
         // end
-        this.node.setPosition(this._targetPos) // 强制位置到终点
-        this._startJump = false // 清理跳跃标记
+        this.node.setPosition(this._targetPos); // 强制位置到终点
+        this._startJump = false; // 清理跳跃标记
       } else {
         // tween
-        this.node.getPosition(this._curPos)
-        this._deltaPos.x = this._curJumpSpeed * deltaTime //每一帧根据速度和时间计算位移
-        Vec3.add(this._curPos, this._curPos, this._deltaPos) // 应用这个位移
-        this.node.setPosition(this._curPos) // 将位移设置给角色
+        this.node.getPosition(this._curPos);
+        this._deltaPos.x = this._curJumpSpeed * deltaTime; //每一帧根据速度和时间计算位移
+        Vec3.add(this._curPos, this._curPos, this._deltaPos); // 应用这个位移
+        this.node.setPosition(this._curPos); // 将位移设置给角色
       }
     }
   }
   onMouseUp(event: EventMouse) {
-    this.jumpByStep(1)
+    this.jumpByStep(1);
   }
   jumpByStep(step) {
-    if (this._startJump) return
-    this._startJump = true
-    this._jumpStep = step
-    this._curJumpTime = 0
-    this._curJumpSpeed = (this._jumpStep * BLOCK_SIZE) / this._jumpTime
-    this.node.getPosition(this._curPos)
-    Vec3.add(this._targetPos, this._curPos, new Vec3(this._jumpStep * BLOCK_SIZE, 0, 0))
+    if (this._startJump) return;
+    this._startJump = true;
+    this._jumpStep = step;
+    this._curJumpTime = 0;
+    this._curJumpSpeed = (this._jumpStep * BLOCK_SIZE) / this._jumpTime;
+    this.node.getPosition(this._curPos);
+    Vec3.add(
+      this._targetPos,
+      this._curPos,
+      new Vec3(this._jumpStep * BLOCK_SIZE, 0, 0)
+    );
   }
 }
